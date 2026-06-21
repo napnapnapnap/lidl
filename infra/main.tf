45f35cd3a8638b8462e8ef78039bf4407a572da5
Dnd-Nap <57704273+Dnd-Nap@users.noreply.github.com>
2026-06-21 13:55:37 +0200

    feat(infra): gce vm, static ip, firewall, conditional disk

--- infra/main.tf
+++ infra/main.tf
@@ -17,3 +17,84 @@ provider "google" {
   project = var.project_id
   zone    = var.zone
 }
+
+locals {
+  region = join("-", slice(split("-", var.zone), 0, 2))
+}
+
+resource "google_compute_address" "vm_ip" {
+  name   = "lidl-bot-ip"
+  region = local.region
+}
+
+resource "google_compute_firewall" "ssh" {
+  name    = "lidl-bot-allow-ssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["lidl-bot"]
+}
+
+resource "google_compute_disk" "data" {
+  count = var.existing_data_disk_name == "" ? 1 : 0
+  name  = "lidl-bot-data"
+  size  = 10
+  type  = "pd-standard"
+  zone  = var.zone
+}
+
+data "google_compute_disk" "existing" {
+  count = var.existing_data_disk_name != "" ? 1 : 0
+  name  = var.existing_data_disk_name
+  zone  = var.zone
+}
+
+locals {
+  data_disk_self_link = (
+    var.existing_data_disk_name != ""
+    ? data.google_compute_disk.existing[0].self_link
+    : google_compute_disk.data[0].self_link
+  )
+}
+
+resource "google_compute_instance" "vm" {
+  name         = "lidl-bot"
+  machine_type = "e2-micro"
+  zone         = var.zone
+  tags         = ["lidl-bot"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+      size  = 20
+    }
+  }
+
+  attached_disk {
+    source      = local.data_disk_self_link
+    device_name = "data"
+    mode        = "READ_WRITE"
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.vm_ip.address
+    }
+  }
+
+  metadata = {
+    ssh-keys       = "debian:${var.ssh_public_key}"
+    startup-script = file("${path.module}/startup.sh")
+  }
+
+  # Prevent Terraform from re-running startup-script on every plan
+  lifecycle {
+    ignore_changes = [metadata["startup-script"]]
+  }
+}
